c74f99e4e730245c3be65d125dbb26db98e36c63
rgosens2 <rgosens2@gmail.com>
2018-05-16 18:47:20 +0200

    commit4

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main ()
+{
+    //qqq
+    int x = 10;
+    int y = 20;
+    std::cout << "Hello, world!" << std::endl;
+
+    int z = 666;
+
+    // Test:
+    int git1 = 77;
+
+    int test1 = 1'000'000;
+    int test2 = 0b1010'1111'0000'0101;
+
+
+    //system("pause");
+}
